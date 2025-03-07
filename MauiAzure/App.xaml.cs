358b60a2ef8b83105e05b591b7a9a1ecda0139d4
Jeff <jeffbohn@my.cwi.edu>
2025-03-06 18:49:12 -0700

    Add project files.

---
+++ MauiAzure/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAzure
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
